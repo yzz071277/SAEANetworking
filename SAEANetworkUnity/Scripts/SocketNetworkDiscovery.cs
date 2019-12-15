47336473b2ae80ce8e47a181ae38738a78073242
Yzz <46965656+yzz071277@users.noreply.github.com>
2019-12-15 17:31:17 +0800

    Unity中对于SAEANetworking的支持

---
+++ SAEANetworkUnity/Scripts/SocketNetworkDiscovery.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+
+/// <summary>
+/// 基于Unity的网络发现类
+/// </summary>
+public class SocketNetworkDiscovery : MonoBehaviour
+{
+    private static SocketNetworkDiscovery Instance { get; set; }
+    private SocketNetworkDiscovery() { }
+    void Awake()
+    {
+        Instance = this;
+    }
+
+
+    public int netDisPort = 10010;
+    public float broadcastInterval = 1f;
+    public float listenTimeout = 4f;
+    public bool isDiscovery = true;
+    private bool isListen;
+
+    Thread thread;
+
+
+    SAEANetworkDiscovery networkDiscovery = new SAEANetworkDiscovery();
+
+
+    private void Start()
+    {
+        if (isDiscovery)
+        {
+            networkDiscovery.OnReceiveBroadcast += OnReceiveBroadcast;
+            networkDiscovery.OnTimeOut += OnTimeOut;
+            networkDiscovery.CreateBroadcastReceivingEnd(netDisPort);
+            networkDiscovery.StartReceiveBroadcast(listenTimeout);
+            isListen = true;
+        }
+    }
+
+
+    /// <summary>
+    /// 当收听到广播时
+    /// </summary>
+    /// <param name="bytes"></param>
+    private void OnReceiveBroadcast(byte[] bytes)
+    {
+        isListen = false;
+        networkDiscovery.StopReceiveBroadcast();
+        CreateClient(SAEAMessageTools.DeserializeByteToString(bytes, 0).Split('|')[1]);
+    }
+
+    /// <summary>
+    /// 当超时时执行的事件
+    /// </summary>
+    private void OnTimeOut() {
+        if (isListen)
+        {
+            CreateServer(SAEASocketManager.GetAddress());
+            networkDiscovery.CreateBroadcastSendingEnd(netDisPort);
+            networkDiscovery.StartBroadcast(broadcastInterval);
+            isListen = false;
+        }
+    }
+
+    /// <summary>
+    /// 创建服务器方法
+    /// </summary>
+    private void CreateServer(string serverIP) {
+        SocketNetworkManager.Instance.serverIP = serverIP;
+        thread = new Thread(SocketNetworkManager.Instance.CreateServer);
+        thread.Start();
+    }
+
+    /// <summary>
+    /// 创建客户端方法
+    /// </summary>
+    private void CreateClient(string serverIP) {
+        SocketNetworkManager.Instance.serverIP = serverIP;
+        SocketNetworkManager.Instance.CreateClient();
+    }
+
+
+
+    private void OnDestroy()
+    {
+        networkDiscovery.CloseBroadcastReceivingEnd();
+        networkDiscovery.CloseBroadcastSendingEnd();
+        if (thread != null && thread.IsAlive)
+        {
+            thread.Abort();
+        }
+    }
+
+    /// <summary>
+    /// 停止发送广播
+    /// </summary>
+    public void StopSendBroadcast()
+    {
+        networkDiscovery.StopBroadcast();
+    }
+
+    /// <summary>
+    /// 停止监听广播
+    /// </summary>
+    public void StopListenBroadcast()
+    {
+        networkDiscovery.StopReceiveBroadcast();
+    }
+
+
+}
---
+++ SAEANetworkUnity/Scripts/SocketNetworkManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System;
+
+/// <summary>
+/// 网络消息协议
+/// </summary>
+public enum NetworkMessageProtocol {
+    AllotClientID, //分配客户端ID
+    SyncWorldAnchor, //同步空间锚
+}
+
+
+public class SocketNetworkManager : MonoBehaviour
+{
+    public static SocketNetworkManager Instance { get; private set; }
+
+    void Awake()
+    {
+        Instance = this; 
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public string serverIP = "127.0.0.1";
+    public int serverPort = 10086;
+
+    public int receiveBuffer = 1024;
+    public int sendBuffer = 1024;
+
+    public int maxClient = 4;
+
+    public bool IsHost { get; private set; }
+    public bool IsServer { get; private set; }
+    public bool IsClient { get; private set; }
+
+    public SAEAServer SocketServer { get; private set;}
+    public SAEAClient SocketClient { get; private set; }
+
+    /// <summary>
+    /// 创建主机(Server + Client)
+    /// </summary>
+    /// <param name="serverIP"></param>
+    /// <param name="ServerPort"></param>
+    public void CreateHost() {
+        CreateServer();
+        CreateClient();
+        IsServer = false;
+        IsClient = false;
+        IsHost = true;
+    }
+
+    /// <summary>
+    /// 作为服务器创建
+    /// </summary>
+    /// <param name="serverIP"></param>
+    /// <param name="serverPort"></param>
+    /// <param name="acceptCallback"></param>
+    public void CreateServer() {
+        SocketServer = new SAEAServer(serverIP, serverPort, receiveBuffer, sendBuffer, maxClient);
+        SocketServer.CreateServer();
+        IsServer = true;
+    }
+
+    /// <summary>
+    /// 作为客户端创建
+    /// </summary>
+    /// <param name="serverIP"></param>
+    /// <param name="serverPort"></param>
+    /// <param name="connectCallback"></param>
+    public void CreateClient() {
+        SocketClient = new SAEAClient(serverIP, serverPort, receiveBuffer, sendBuffer);
+        SocketClient.ClientConnect(); 
+        IsClient = true;
+    }
+
+
+    private void OnDestroy()
+    {
+        if (SocketClient != null)
+        {
+            SocketClient.SAEAClose();
+        }
+        if (SocketServer != null)
+        {
+            SocketServer.SAEAClose();
+        }
+    }
+
+
+
+}
